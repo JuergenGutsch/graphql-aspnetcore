8243f3bdfb7d3fd84f44d2446e162f8a54a940ec
JuergenGutsch <juergen@gutsch-online.de>
2018-02-21 21:17:19 +0100

    Fix #16: Error with mutations with complex input types

--- GraphQl.AspNetCore/GraphQlParameters.cs
+++ GraphQl.AspNetCore/GraphQlParameters.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using GraphQL;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace GraphQl.AspNetCore
 {
@@ -11,7 +14,35 @@ namespace GraphQl.AspNetCore
 
         public Inputs GetInputs()
         {
-            return (Variables != null) ? new Inputs(Variables) : new Inputs();
+            Dictionary<string, object> sanitizedVariables = null;
+
+            if (Variables != null && Variables.Any())
+            {
+                sanitizedVariables = new Dictionary<string, object>();
+
+                foreach (var key in Variables.Keys)
+                {
+                    var value = Variables[key];
+
+                    if (value is JObject)
+                    {
+                        // fix the nesting ( {{ }} )
+                        var serialized = JsonConvert.SerializeObject(value);
+                        var deserialized = JsonConvert.DeserializeObject<Dictionary<string, object>>(serialized);
+                        sanitizedVariables.Add(key, deserialized);
+                    }
+                    else
+                    {
+                        sanitizedVariables.Add(key, value);
+                    }
+                }
+
+                return new Inputs(sanitizedVariables);
+            }
+            else
+            {
+                return (Variables != null) ? new Inputs(Variables) : new Inputs();
+            }
         }
     }
 }
