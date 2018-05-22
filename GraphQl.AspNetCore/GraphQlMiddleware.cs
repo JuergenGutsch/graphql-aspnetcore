5219b522856a3c305e7a41d5104fe897c8aa926b
Tigran Arakelyan <tigran@hexdivision.com>
2018-05-22 22:15:17 +0400

    [FEAT] Implement multipart file upload

--- GraphQl.AspNetCore/GraphQlMiddleware.cs
+++ GraphQl.AspNetCore/GraphQlMiddleware.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using GraphQL;
 using GraphQL.Execution;
@@ -11,9 +9,12 @@ using GraphQL.Types;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace GraphQl.AspNetCore
 {
@@ -51,10 +52,18 @@ namespace GraphQl.AspNetCore
             HttpResponse response = httpContext.Response;
 
             // GraphQL HTTP only supports GET and POST methods.
-            if (request.Method != "GET" && request.Method != "POST")
+            if (request.Method != HttpMethods.Get && request.Method != HttpMethods.Post)
             {
-                response.Headers.Add("Allow", "GET, POST");
-                response.StatusCode = 405;
+                // Handle CORS issue
+                if (request.Method == HttpMethods.Options)
+                {
+                    response.StatusCode = StatusCodes.Status200OK;
+                    response.ContentType = "application/json; charset=utf-8";
+
+                    return;
+                }
+
+                response.StatusCode = StatusCodes.Status405MethodNotAllowed;
 
                 return;
             }
@@ -86,6 +95,8 @@ namespace GraphQl.AspNetCore
                 options.CancellationToken = httpContext.RequestAborted;
                 options.ComplexityConfiguration = _options.ComplexityConfiguration;
                 options.UserContext = httpContext;
+                options.Root = httpContext;
+
                 options.ExposeExceptions = _options.ExposeExceptions;
                 ConfigureDocumentExecutionListeners(options, _executionListeners);
             });
@@ -98,7 +109,7 @@ namespace GraphQl.AspNetCore
             var writer = new DocumentWriter(indent: _options.FormatOutput);
             var json = writer.Write(result);
 
-            response.StatusCode = 200;
+            response.StatusCode = StatusCodes.Status200OK;
             response.ContentType = "application/json; charset=utf-8";
 
             await response.WriteAsync(json);
@@ -106,46 +117,83 @@ namespace GraphQl.AspNetCore
 
         private static async Task<GraphQlParameters> GetParametersAsync(HttpRequest request)
         {
-            // http://graphql.org/learn/serving-over-http/#http-methods-headers-and-body
+            GraphQlParameters parameters = null;
 
-            string body = null;
-            if (request.Method == "POST")
+            // http://graphql.org/learn/serving-over-http/#http-methods-headers-and-body
+            if (request.Method == HttpMethods.Post)
             {
-                // Read request body
-                using (var sr = new StreamReader(request.Body))
+                MediaTypeHeaderValue.TryParse(request.ContentType, out MediaTypeHeaderValue contentType);
+
+                switch (contentType.MediaType.Value)
                 {
-                    body = await sr.ReadToEndAsync();
+                    case "application/json":
+                        // Parse request as json
+                        var bodyJson = await request.ReadAsString();
+                        parameters = JsonConvert.DeserializeObject<GraphQlParameters>(bodyJson);
+                        break;
+
+                    case "application/graphql":
+                        // The whole body is the query
+                        var bodyGraphQL = await request.ReadAsString();
+                        parameters = new GraphQlParameters { Query = bodyGraphQL };
+                        break;
+                    case "multipart/form-data":
+                        parameters = await GetGraphQLParametersFromMultipartBody(request, contentType);
+                        break;
+                    default:
+                        // Don't parse anything
+                        parameters = new GraphQlParameters();
+                        break;
+                }
+
+                string query = request.Query["query"];
+
+                // Query string "query" overrides a query in the body
+                parameters.Query = query ?? parameters.Query;
+            }
+
+            return parameters;
+        }
+
+        // Based on protocol https://github.com/jaydenseric/graphql-multipart-request-spec
+        private static async Task<GraphQlParameters> GetGraphQLParametersFromMultipartBody(HttpRequest request, MediaTypeHeaderValue contentType)
+        {
+            var graphqlBody = string.Empty;
+
+            var formAccumulator = default(KeyValueAccumulator);
+
+            var boundary = MultipartRequestHelper.GetBoundary(contentType);
+
+            using (var sr = request.ReadAsStream())
+            {
+                var reader = new MultipartReader(boundary, sr);
+
+                var section = await reader.ReadNextSectionAsync();
+
+                while (section != null)
+                {
+                    var hasContentDispositionHeader =
+                        ContentDispositionHeaderValue.TryParse(
+                            section.ContentDisposition,
+                            out ContentDispositionHeaderValue contentDisposition);
+
+                    if (hasContentDispositionHeader)
+                    {
+                        if (contentDisposition.IsFormDisposition())
+                        {
+                            formAccumulator = await MultipartRequestHelper.AccumulateForm(formAccumulator, section, contentDisposition);
+                        }
+                    }
+
+                    section = await reader.ReadNextSectionAsync();
                 }
             }
 
-            MediaTypeHeaderValue.TryParse(request.ContentType, out MediaTypeHeaderValue contentType);
+            var formResults = formAccumulator.GetResults();
+            var operations = formResults.GetPropertyValue("operations");
+            var variables = formResults.GetPropertyValue("map");
 
-            GraphQlParameters parameters;
-
-            switch (contentType.MediaType)
-            {
-                case "application/json":
-                    // Parse request as json
-                    parameters = JsonConvert.DeserializeObject<GraphQlParameters>(body);
-                    break;
-
-                case "application/graphql":
-                    // The whole body is the query
-                    parameters = new GraphQlParameters {Query = body};
-                    break;
-
-                default:
-                    // Don't parse anything
-                    parameters = new GraphQlParameters();
-                    break;
-            }
-
-            string query = request.Query["query"];
-
-            // Query string "query" overrides a query in the body
-            parameters.Query = query ?? parameters.Query;
-
-            return parameters;
+            return new GraphQlParameters { Query = operations.ToString(), Variables = JObject.Parse(variables.ToString()) };
         }
 
         private static void ConfigureDocumentExecutionListeners(ExecutionOptions options,
