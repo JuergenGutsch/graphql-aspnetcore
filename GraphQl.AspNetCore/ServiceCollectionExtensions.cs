e7b4068cea1784b3bdcb60def26739d55e1117cf
Juergen Gutsch <juergen@gutsch-online.de>
2018-10-03 13:49:10 +0200

    Revert "Add DocumentExecutor injection"
    This reverts commit 88012e2a0817a9b0e64a396bcaf2895272a8979c.

--- GraphQl.AspNetCore/ServiceCollectionExtensions.cs
+++ GraphQl.AspNetCore/ServiceCollectionExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using GraphQl.AspNetCore;
-using GraphQL;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -20,8 +19,6 @@ namespace Microsoft.Extensions.DependencyInjection
             if (configure == null)
                 throw new ArgumentNullException(nameof(configure));
 
-            services.AddScoped<DocumentExecuter>();
-            
             var builder = new GraphQlBuilder(services);
 
             var schema = new SchemaConfiguration(null);
