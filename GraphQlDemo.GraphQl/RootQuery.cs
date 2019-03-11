91a9a1088d809b67a82dbb1a0333d0133b81d3b1
Lex Tas <lextas@outlook.com>
2019-03-11 20:35:06 +0100

    Added GraphQL implementations

--- GraphQlDemo.GraphQl/RootQuery.cs
+++ GraphQlDemo.GraphQl/RootQuery.cs
@@ -6,9 +6,14 @@ namespace GraphQlDemo.GraphQl
 {
     public class RootQuery : ObjectGraphType
     {
-        public RootQuery(BookRootTypes bookRootTypes)
+        public RootQuery(
+            BookRootTypes bookRootTypes,
+            AuthorRootTypes authorRootTypes,
+            PublisherRootTypes publisherRootTypes)
         {
             this.AddFields(bookRootTypes.Fields);
+            this.AddFields(authorRootTypes.Fields);
+            this.AddFields(publisherRootTypes.Fields);
         }
     }
 }
