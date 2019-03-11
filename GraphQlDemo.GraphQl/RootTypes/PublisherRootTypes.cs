a61886651aff1fa72d8b942361d6fd53d565297b
Lex Tas <lextas@outlook.com>
2019-03-11 20:35:06 +0100

    Added GraphQL implementations

---
+++ GraphQlDemo.GraphQl/RootTypes/PublisherRootTypes.cs
@@ -0,0 +1,36 @@
+﻿using GraphQL.Types;
+using GraphQlDemo.GraphQl.Types;
+using GraphQlDemo.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphQlDemo.GraphQl.RootTypes
+{
+    public class PublisherRootTypes : ObjectGraphType
+    {
+        public PublisherRootTypes(IPublisherService publisherService)
+        {
+            // get all
+
+            this.FieldAsync<ListGraphType<PublisherType>>(
+                name: "publishers",
+                resolve: async context => await publisherService.GetPublishersAsync()
+            );
+
+            // get by id
+
+            var args = new QueryArguments(
+                new QueryArgument<IdGraphType> { Name = "id" });
+
+            this.FieldAsync<PublisherType>(
+                name: "publisherById",
+                arguments: args,
+                resolve: async context =>
+                {
+                    var id = context.GetArgument<int>("id");
+                    return await publisherService.GetPublisherByIdAsync(id);
+                });
+        }
+    }
+}
